d5874b8e161f08c01d00f2785f40b969757d4d57
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-11-10 18:52:44 +0100

    Finished Naughtylist

--- Övningsuppgifter/Mockito/NaughtyList/src/main/java/org/example/NaughtyListTest.java
+++ Övningsuppgifter/Mockito/NaughtyList/src/main/java/org/example/NaughtyListTest.java
@@ -29,28 +29,15 @@ public class NaughtyListTest {
         when(naughtyListMock.getChildList()).thenReturn(childList);
 
         String actual = null;
+        String expected = "Charlie";
 
-        //Todo: METHOD 1: Find out if data is supposed to be collected in THIS method OR (METHOD 2) mocked class' method
+        //Todo: Find out if standard/optimal way to get data
         for (Child item: naughtyListMock.getChildList()) {
             if (item.getPersonalNumber() == 1001){
                 actual = item.getName();
             }
         }
 
-        assertEquals("Charlie", actual);
-    }
-
-    /*@Test
-    public void getNaughtyChild(){
-        ArrayList<Child> childList =
-                new ArrayList<>(List.of(new Child("Charlie", 1001, true), new Child("Jeremy", 1002, false)));
-        when(naughtyListMock.getChildList()).thenReturn(childList);
-        System.out.println(childList.get(0).getName());
-
-        System.out.println(naughtyListMock.getChild(1001));
-
-        String expected = "Charlie";
-        String actual = naughtyListMock.getChild(1001).getName();
         assertEquals(expected, actual);
-    }*/
+    }
 }
