84bb56d77c4517348b466ef9c1851c3ab61232c1
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-11-10 19:14:24 +0100

    Added dependencies

---
+++ Övningsuppgifter/Mockito/2_to_5/MockitoTests/src/main/java/org/example/TestClass.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.mockito.Mockito;
+
+public class TestClass {
+
+    @BeforeEach
+    public void setup(){
+
+    }
+
+}
