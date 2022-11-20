a807a1e18f147c029c0149932dd752485f971ee5
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-11-20 12:54:25 +0100

    Added search and borrow service

--- Uppgifter/LibraryManager/src/main/java/org/example/services/LibraryService.java
+++ Uppgifter/LibraryManager/src/main/java/org/example/services/LibraryService.java
@@ -2,6 +2,12 @@ package org.example.services;
 
 public class LibraryService {
 
+    private BorrowService borrowService;
+    private SearchService searchService;
+    private static final int borrowPrice = 75;
 
-
+    public LibraryService(BorrowService borrowService, SearchService searchService) {
+        this.borrowService = borrowService;
+        this.searchService = searchService;
+    }
 }
