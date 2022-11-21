f6aad8adde1288daefdcf322058378c7d0aa838a
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-11-21 13:58:31 +0100

    Update LibraryService.java

--- Uppgifter/LibraryManager/src/main/java/org/example/services/LibraryService.java
+++ Uppgifter/LibraryManager/src/main/java/org/example/services/LibraryService.java
@@ -1,13 +1,19 @@
 package org.example.services;
 
+import org.example.Book;
+
+import java.util.ArrayList;
+
 public class LibraryService {
 
     private BorrowService borrowService;
     private SearchService searchService;
+    private ArrayList<Book> storedBooks;
     private static final int borrowPrice = 75;
 
-    public LibraryService(BorrowService borrowService, SearchService searchService) {
+    public LibraryService(BorrowService borrowService, SearchService searchService, ArrayList<Book> storedBooks) {
         this.borrowService = borrowService;
         this.searchService = searchService;
+        this.storedBooks = storedBooks;
     }
 }
