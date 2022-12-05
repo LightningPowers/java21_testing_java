d903643be172412e05a373575a8d450fedca1c96
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-12-05 09:19:55 +0100

    Added second assignment

---
+++ Uppgifter/Coordinates/src/main/java/org/example/Coordinates.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Coordinates {
+
+    private double x;
+    private double y;
+    private double z;
+
+    public Coordinates(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+}
